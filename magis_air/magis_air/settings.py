c0ac30253b95f7a7c3953dd3f15f07d55c388c76
Sophia Salta <sophia.salta@student.ateneo.edu>
2024-11-13 22:09:33 +0800

    removed edits from db

--- magis_air/magis_air/settings.py
+++ magis_air/magis_air/settings.py
@@ -85,12 +85,8 @@ WSGI_APPLICATION = 'magis_air.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': 'EirYtSHoAYwcxPisHZPtXXrlnIhAzqjV',
-        'HOST': 'postgres.railway.internal',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
