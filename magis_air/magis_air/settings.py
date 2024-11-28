cd2c76787f626b082a9075e83ffb290e393893a8
Sophia Salta <sophia.salta@student.ateneo.edu>
2024-11-29 07:11:59 +0800

    merged sql n django

--- magis_air/magis_air/settings.py
+++ magis_air/magis_air/settings.py
@@ -83,10 +83,15 @@ WSGI_APPLICATION = 'magis_air.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# psql -U postgres -d "magis-air"
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'magis-air',
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '4321'),
     }
 }
 
